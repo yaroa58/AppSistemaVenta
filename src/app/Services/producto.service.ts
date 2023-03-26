2a1825299cebf810c3a0dfe19f64dffcaf7e4c96
Yeison Roa <alexandr.ortega@hotmail.com>
2023-03-25 21:02:06 -0500

    Add ejecucion de metodos de Productos

--- src/app/Services/producto.service.ts
+++ src/app/Services/producto.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs'; //permite recibir las respuestas de las Apis
+import { environment } from 'src/environments/environment'; //url de la api rest
+import { ResponseApi } from '../Interfaces/response-api'; // permite recibir la respuesta de las solicitudes Http
+import { Producto } from '../Interfaces/producto';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
+  private urlApi: string = environment.endpoint + "Categoria/"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //metodo para obtener lista de usuarios
+  lista(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
+  }
+  // Metodo para guardar Producto
+  guardar(request: Producto): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.urlApi}Guardar`, request)
+  }
+  // Metodo para editar Producto
+  editar(request: Producto): Observable<ResponseApi> {
+    return this.http.put<ResponseApi>(`${this.urlApi}Editar`, request)
+  }
+  // Metodo para eliminar Producto
+  eliminar(id: number): Observable<ResponseApi> {
+    return this.http.delete<ResponseApi>(`${this.urlApi}Eliminar/${id}`)
+  }
 }
