2e3fd46dd938357c29ae3e71d521b69dead5438d
Yeison Roa <alexandr.ortega@hotmail.com>
2023-03-25 20:51:57 -0500

    Add ejecucion de metodos de Rol

--- src/app/Services/rol.service.ts
+++ src/app/Services/rol.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs'; //permite recibir las respuestas de las Apis
+import { environment } from 'src/environments/environment'; //url de la api rest
+import { ResponseApi } from '../Interfaces/response-api'; // permite recibir la respuesta de las solicitudes Http
+import { Login } from '../Interfaces/login'; // importacion de interfaces para recibir las credenciales
+import { Usuario } from '../Interfaces/usuario'; // interfaz de usuario
+
 @Injectable({
   providedIn: 'root'
 })
 export class RolService {
+  private urlApi: string = environment.endpoint + "Rol/";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //metodo para obtener lista de usuarios
+  lista(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
+  }
 }
