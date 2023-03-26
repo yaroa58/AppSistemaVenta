40ff9ecbdb453d6c439ff75a2deb053edf14b88a
Yeison Roa <alexandr.ortega@hotmail.com>
2023-03-25 20:57:46 -0500

    Add ejecucion de metodos de Categoria

--- src/app/Services/categoria.service.ts
+++ src/app/Services/categoria.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs'; //permite recibir las respuestas de las Apis
+import { environment } from 'src/environments/environment'; //url de la api rest
+import { ResponseApi } from '../Interfaces/response-api'; // permite recibir la respuesta de las solicitudes Http
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
+  private urlApi: string = environment.endpoint + "Categoria/"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //metodo para obtener lista de usuarios
+  lista(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
+  }
 }
