adb251b9b0d741000dc643652caddd387e14bf4c
Yeison Roa <alexandr.ortega@hotmail.com>
2023-03-25 21:13:13 -0500

    Add ejecucion de metodos de DashBoard

--- src/app/Services/dash-board.service.ts
+++ src/app/Services/dash-board.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs'; //permite recibir las respuestas de las Apis
+import { environment } from 'src/environments/environment'; //url de la api rest
+import { ResponseApi } from '../Interfaces/response-api'; // permite recibir la respuesta de las solicitudes Http
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class DashBoardService {
 
-  constructor() { }
+  private urlApi: string = environment.endpoint + "DashBoard/"
+
+  constructor(private http: HttpClient) { }
+
+  //metodo para obtener lista de Dashboard
+  resumen(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlApi}Resumen`)
+  }
+
 }
